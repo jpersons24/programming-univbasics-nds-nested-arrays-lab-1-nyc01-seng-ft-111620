36a8a1d4f9c692ed1c9902e9ecfea05447337817
Jakob Persons <jakobep24@gmail.com>
2020-10-21 15:00:02 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -4,7 +4,7 @@
 #   ["Paprika", "Fajita Mix", "Coriander"],   # 1
 #   ["Parsley", "Sage", "Rosemary"]     # 2
 # ]
-# 
+#
 # spice_rack[0]  #=> ["Mace", "Ginger", "Marjoram"]
 # spice_rack[0][1]  #=> "Ginger"
 # spice_rack[1][1]   #=>  "Fajita Mix"
@@ -51,7 +51,7 @@ def sorted_matrix
   # Using Array literal syntax only, build another nested array that
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
-  
+
 end
 
 def matrix_lookup(matrix, row, column)
